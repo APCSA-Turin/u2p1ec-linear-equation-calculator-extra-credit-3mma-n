3ca7fd3a14cc3a7ce256b0a96ecd4e5b6530efa7
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-10-17 16:35:12 +0000

    10/17

--- src/main/java/com/example/project/LinearCalculator.java
+++ src/main/java/com/example/project/LinearCalculator.java
@@ -3,7 +3,99 @@ public class LinearCalculator{
     //FOR EXTRA CREDIT 
     //you should copy and paste all of your code from the LinearCalculator class
     // but NOT printInfo(). Please update it below
+    //INSTANCE VARIABLES 
+    //4 INTEGER variables (name them: x1,x2,y1,y2) 
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
 
+    //CONSTRUCTOR
+    //1 constructor with 2 String parameters. Each parameter represents a coordinate. 
+    //For example, "(1,2)" and "(3,4)" would be two parameter values 
+    //You will have to parse the string into 4 integers, representing the 2 points.
+    public LinearCalculator(String coord1, String coord2){ // <--add 2 string parameters to this constructor
+        int idx = coord1.indexOf(","); // finds the index of the comma in the coordinate pair
+        x1 = Integer.parseInt(coord1.substring(1, idx)); // picks out characters between the open parenthesis and the comma
+        y1 = Integer.parseInt(coord1.substring(idx + 1, coord1.length() - 1)); // picks out characters after the comma and before the closing parenthesis
+        idx = coord2.indexOf(","); // process is repeated for coord2
+        x2 = Integer.parseInt(coord2.substring(1, idx)); 
+        y2 = Integer.parseInt(coord2.substring(idx + 1, coord2.length() - 1)); 
+    }
+
+
+
+    //METHODS
+    //getters and setters for the 4 instance variables (8 methods total)
+    public int getX1(){return x1;}
+    public int getY1(){return y1;}
+    public int getX2(){return x2;}
+    public int getY2(){return y2;}
+    public void setX1(int newVal){x1 = newVal;}
+    public void setY1(int newVal){y1 = newVal;}
+    public void setX2(int newVal){x2 = newVal;}
+    public void setY2(int newVal){y2 = newVal;}
+
+
+    //distance() -> returns a double. 
+    //calculates the distance between the two points to the nearest HUNDREDTH and returns the value.
+    public double distance(){
+        double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)); // calculates distance using distance formula
+        return roundedToHundredth(dist);
+    }
+
+    //yInt() -> returns a double.
+    //calculates the y intercept of the equation and returns the value to the nearest HUNDREDTH
+    //if y-int if undefined, should return -999.99
+    public double yInt(){
+        if ((x2 - x1) != 0) {
+            return roundedToHundredth(y1 - (x1 * slope()));
+        }
+        return -999.99;
+    }
+
+    //slope() -> returns a double. 
+    //calculates the slope of the equations and returns the value to the nearest HUNDREDTH
+    //if slope is undefined, should return -999.99
+    public double slope(){
+        if ((x2 - x1) != 0) {
+            return roundedToHundredth((double) (y2 - y1) / (x2 - x1));
+        }
+        return -999.99;
+    }
+
+    //equations() -> returns a String.
+    //calculates the final equation in y=mx+b form and returns the string
+    //if the equation has no slope, the equation should return -> "undefined"
+    //HINT: You may need other custom methods to decrease the amount of code in the equations() method
+    public String equation(){
+        double slope = slope();
+        double yInt = yInt(); 
+        if (slope == -999.99) {
+            return "undefined";
+        }
+        String str = "y=";
+        if (slope != 0) {
+            str += slope + "x";
+
+            if (yInt > 0) {
+                str += "+";
+            }
+        }
+        if (yInt != 0) {
+            str += yInt;
+        }
+        return str;
+    }
+
+    //roundedToHundredth(double x)-> returns double
+    //calculates the input to the nearest hundredth and returns that value
+    public double roundedToHundredth(double x){
+        if (x > 0) {
+            return (int) (x * 100 + 0.5) / 100.0;
+        }
+        return (int) (x * 100 - 0.5) / 100.0;
+    }
 
     //You will need to concatenate to the string 
     //the results from findSymmetry() and Midpoint()
@@ -20,14 +112,31 @@ public class LinearCalculator{
     //return "Symmetric about the origin";
     //return "No symmetry";
     public String findSymmetry(){
-        return "";
+        boolean xEqual = (x1 == x2);
+        boolean yEqual = (y1 == y2);
+        boolean xNeg = (x1 * -1 == x2);
+        boolean yNeg = (y1 * -1 == y2);
+        if (xNeg && yNeg) {
+            return "Symmetric about the origin";
+        }
+        if (xNeg && yEqual) {
+            return "Symmetric about the x-axis";
+        }
+        if (xEqual && yNeg) {
+            return "Symmetric about the origin";
+        }
+        else {
+            return "No symmetry";
+        }
     }
 
     //Midpoint()->return a string 
     //the method should calculate the midpoint between the two points
     //it should return "The midpoint of this line is: (0,0)";
     public String Midpoint(){
-        return "";
+        double midX = (x1 + x2) / 2.0;
+        double midY = (y1 + y2) / 2.0;
+        return "The midpoint of this line is: (" + midX + "," + midY + ")";
     }
 
 }
